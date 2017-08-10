6485e36fe31dc57d138fcd8c906fcdad69c6b609
Basel Shbita <basel921@gmail.com>
2017-08-11 02:01:53 +0300

    add profile pic path to users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,9 @@ module UsersHelper
   # Returns the profile picture for the given user.
   def profile_pic_for(user, options = { size: 80 })
     pic_url = "profile_empty.jpg"
+    if user.profile_pic_path?
+      pic_url = user.profile_pic_path
+    end
     image_tag(pic_url,
               alt: user.name,
               width: options[:size],
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809163224) do
+ActiveRecord::Schema.define(version: 20170810164408) do
 
   create_table "stories", force: :cascade do |t|
     t.text "content", limit: 512
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170809163224) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "profile_pic_path"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
