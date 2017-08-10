79f91341ad756da671f76f90b44331ae8375da63
Basel Shbita <basel921@gmail.com>
2017-08-10 03:46:43 +0300

    add some authorizations

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  # Returns the profile picture for the given user.
+  def profile_pic_for(user, options = { size: 80 })
+    pic_url = "profile_empty.jpg"
+    image_tag(pic_url,
+              alt: user.name,
+              width: options[:size],
+              height: options[:size])
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:clark)
+    @other_user = users(:bruce)
   end
 
   test "should get index" do
+    log_in_as(@user)
     get users_url
     assert_response :success
   end
@@ -33,11 +35,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    log_in_as(@user)
     get edit_user_url(@user)
     assert_response :success
   end
 
   test "should update user" do
+    log_in_as(@user)
     patch user_url(@user), params: { user: { email: @user.email,
                                              name: @user.name,
                                              password: 'password',
@@ -50,5 +54,38 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_url(@user)
     end
     assert_redirected_to users_url
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 end
