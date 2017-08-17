c584124bed641bde45f512fc562444db4287d2a3
Basel Shbita <basel921@gmail.com>
2017-08-17 10:23:22 +0300

    generalizing the authenticated? method and add activation edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,9 +29,6 @@ class UsersController < ApplicationController
       UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
-      #log_in @user
-      #flash[:success] = "Welcome to the Mainisi!"
-      #redirect_to @user
     else
       render :new
     end
