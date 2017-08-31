aa81746595685339d9bc727253e08a967a20d9ea
Basel Shbita <basel921@gmail.com>
2017-08-31 03:52:24 +0300

    add password_reset attribute, edit mailer & update digest in db

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :stories,             only: [:index, :show, :create, :destroy]
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
