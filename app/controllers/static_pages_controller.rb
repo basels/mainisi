deac840224efca70a0e0dd7b8fcfebf24873d65c
Basel Shbita <basel921@gmail.com>
2017-08-31 16:10:52 +0300

    Added contact page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,12 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+
+  def contact
+    @linkedin_url = 'https://il.linkedin.com/in/basel-shbita-8294b077'
+    @github_url = 'https://github.com/basels'
+    @facebook_url = 'https://www.facebook.com/basel92'
+    @instagram_url = 'https://www.instagram.com/basel92'
+    @soundcloud_url = 'https://soundcloud.com/basels'
+  end
 end
